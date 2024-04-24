09f68e783453f9b8fa61c292f615aa68b43c3351
Ogojoshua905 <joshuaexcellency1@gmail.com>
2024-04-24 19:50:02 +0100

    feat: create platform

---
+++ next.config.js
@@ -0,0 +1,68 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'www.pexels.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: "unsplash.com",
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
+  
+  module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
+
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: "www.unsplash.com",
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
+
+  
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.pexels.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Acme } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Acme({ subsets: ["latin"], 
+                      weight: ['400']});
 
 export const metadata = {
   title: "Create Next App",
