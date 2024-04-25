a8095c94a2a3bdebbe150d931a3ea65c2f441f94
Ogojoshua905 <joshuaexcellency1@gmail.com>
2024-04-25 12:29:13 +0100

    fix:made changes

--- src/app/comps/Footer.jsx
+++ src/app/comps/Footer.jsx
@@ -1,34 +1,17 @@
-import Image from 'next/image'
+"use client"
 import SocialIcons  from './SocialIcons'
 import PageLinks from './PageLinks'
-import Link from 'next/link'
+
 
 export default function Footer() {
   return (
    <footer className='h-[100vh] bg-[#111812] text-white flex gap-9'>
     <div className="flex flex-col gap-y-6">
 
-    {/* <p className="animate-left">We are dedicated to providing the best gaming experience for our users.</p> */}
-
-
-    {/* <div className='flex gap-x-2 items-center capitalize'>
-    <Image src="/LOGO2.png" alt='logo' width="100" height='100'/>
-    </div> */}
     <p className='text-3xl'>GamingNow</p>
         <p>Thank you for choosing GamingNow as your go-to <br /> platform for all things gaming</p>
         <SocialIcons />
     </div>
-    
-    {/* <div className='capitalize'>
-      <h2>Category</h2>
-    <div>
-      <CatLinks title="Call Of Duty: Mobile" bg="bg-red-600"/>
-      <CatLinks title="Bloodstrike" bg="bg-red-700"/><br/>
-      <CatLinks title="eFootball" bg="bg-green-700"/>
-      <CatLinks title="react" bg="bg-orange-500"/><br/>
-      <CatLinks  title="tailwindcss" bg="bg-fuchsia-700"/>
-    </div>
-  </div> */}
 
     <div className='flex justify-start items-start gap-5 text-2xl'>
   <div className='capitalize'>
